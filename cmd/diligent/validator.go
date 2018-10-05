697b4fb014d497448a52530b4735cf259e0882d3
Dan Reid <danielallenreid@gmail.com>
2018-10-05 12:29:51 +0100

    detailing all dependencies which do not comply with the whitelist

--- cmd/diligent/validator.go
+++ cmd/diligent/validator.go
@@ -19,17 +19,18 @@ func checkWhitelist() error {
 	for _, w := range licenseWhitelist {
 		_, err := diligent.GetLicenseFromIdentifier(w)
 		if err != nil {
-			return fmt.Errorf("whitelisted license %s is not a known license identifier", w)
+			return fmt.Errorf("whitelisted license '%s' is not a known license identifier", w)
 		}
 	}
 	return nil
 }
 
-func validateDependencies(deps []diligent.Dep) error {
+func validateDependencies(deps []diligent.Dep) []error {
+	ee := make([]error, 0, len(deps))
 	for _, d := range deps {
 		if isInWhitelist(d.License) == false {
-			return fmt.Errorf("dependency %s has license %s which is not in your license whitelist", d.Name, d.License.Identifier)
+			ee = append(ee, fmt.Errorf("dependency '%s' has license '%s' which is not in your license whitelist", d.Name, d.License.Identifier))
 		}
 	}
-	return nil
+	return ee
 }
